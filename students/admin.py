0c5b0c2b66dc84bb2c001fc1f3065d3905b3608b
Mahima-2121 <mahimathakur626120@gmail.com>
2026-06-02 22:04:17 +0530

    Added login feature

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display  = ['roll_number', 'first_name',
+                     'last_name', 'course', 'is_active']
+    search_fields = ['first_name', 'last_name',
+                     'roll_number', 'email']
+    list_filter   = ['course', 'gender', 'is_active']
